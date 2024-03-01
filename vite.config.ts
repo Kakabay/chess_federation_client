c9e4d150e8e022e9c270be662e17d68f858e2685
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-03-01 17:54:34 +0500

    build

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,9 @@ export default defineConfig({
   build: {
     // generate .vite/manifest.json in outDir
     manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: '/path/to/main.js',
-    },
+    // rollupOptions: {
+    //   // overwrite default .html entry
+    //   input: '/path/to/main.js',
+    // },
   },
 });
