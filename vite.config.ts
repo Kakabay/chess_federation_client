600b07aae5645ee899c910129000add524ca976a
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-03-04 14:54:11 +0500

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 // vite.config.js
 export default defineConfig({
   build: {
     // generate .vite/manifest.json in outDir
     manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: '/path/to/main.js',
-    },
+    // rollupOptions: {
+    //   // overwrite default .html entry
+    //   input: '/path/to/main.js',
+    // },
   },
 });
