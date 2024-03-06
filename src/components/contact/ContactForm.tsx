1258cb42661a728793adefe0015ed8307bd43af0
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-03-06 16:20:19 +0500

    build

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -1,60 +1,59 @@
 // Modules
-import React, { useEffect, useState } from "react";
-import Skeleton from "react-loading-skeleton";
+import React, { useEffect, useState } from 'react';
+import Skeleton from 'react-loading-skeleton';
 
 // Images
-import board from "../../images/board.jpg";
+import board from '../../images/board.jpg';
 
 // Helpers
-import { getContact, sendData } from "../../helpers/apiRequests";
-import { highlightColor } from "../../helpers/otherVariables";
+import { getContact, sendData } from '../../helpers/apiRequests';
+import { highlightColor } from '../../helpers/otherVariables';
 
 // Components
-import Loader from "../global/Loader";
-import Status from "../contact/Status";
+import Loader from '../global/Loader';
+import Status from '../contact/Status';
 
 // Validators
-import { validateEmail } from "../../validators/validateEmail";
+import { validateEmail } from '../../validators/validateEmail';
 
 // Icons
-import forward from "../../icons/arrow-forward.svg";
-import email from "../../icons/email-black.svg";
-import phone from "../../icons/phone-gray.svg";
-import pin from "../../icons/pin.svg";
-import fb from "../../icons/facebook-gray.svg";
-import yt from "../../icons/youtube-gray.svg";
-import ig from "../../icons/instagram-grey.svg";
-import twitter from "../../icons/twitter-gray.svg";
+import forward from '../../icons/arrow-forward.svg';
+import email from '../../icons/email-black.svg';
+import phone from '../../icons/phone-gray.svg';
+import pin from '../../icons/pin.svg';
+import fb from '../../icons/facebook-gray.svg';
+import yt from '../../icons/youtube-gray.svg';
+import ig from '../../icons/instagram-grey.svg';
+import twitter from '../../icons/twitter-gray.svg';
 
 // Types
-import { ContactData, Contact } from "../../types/contact";
+import { ContactData, Contact } from '../../types/contact';
 type stateBool = [boolean, React.Dispatch<boolean>];
 
 const ContactForm = () => {
-  const [contact, setContact]: [Contact[], React.Dispatch<Contact[]>] =
-    useState([
-      {
-        id: -1,
-        phone: "",
-        address: "",
-        instagram: "",
-        twitter: "",
-        youtube: "",
-        facebook: "",
-        translations: [
-          {
-            model_id: "",
-            locale: "",
-            attribute_data: "",
-          },
-        ],
-      },
-    ]);
+  const [contact, setContact]: [Contact[], React.Dispatch<Contact[]>] = useState([
+    {
+      id: -1,
+      phone: '',
+      address: '',
+      instagram: '',
+      twitter: '',
+      youtube: '',
+      facebook: '',
+      translations: [
+        {
+          model_id: '',
+          locale: '',
+          attribute_data: '',
+        },
+      ],
+    },
+  ]);
 
   const [data, setData]: [ContactData, React.Dispatch<ContactData>] = useState({
-    name: "",
-    email: "",
-    message: "",
+    name: '',
+    email: '',
+    message: '',
   });
 
   const [alert, setAlert] = useState({
@@ -69,16 +68,12 @@ const ContactForm = () => {
   const [validation, setValidation]: stateBool = useState(false);
 
   const status = {
-    success: "Ваше сообщение успешно отправлено",
-    error: "Не удалось отправить сообщение",
+    success: 'Ваше сообщение успешно отправлено',
+    error: 'Не удалось отправить сообщение',
   };
 
   useEffect(() => {
-    if (
-      data.name.length !== 0 &&
-      validateEmail(data.email) &&
-      data.message.length !== 0
-    ) {
+    if (data.name.length !== 0 && validateEmail(data.email) && data.message.length !== 0) {
       setValidation(true);
     } else {
       setValidation(false);
@@ -89,14 +84,15 @@ const ContactForm = () => {
     getContact(setContact);
   }, []);
 
+  console.log(contact);
+
   return (
     <form
       className="contact-form"
       onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-      }}
-    >
-      <Loader className={load ? "active" : ""} />
+      }}>
+      <Loader className={load ? 'active' : ''} />
       <div className="contact-form-left">
         <div className="contact-img">
           <img src={board} alt="" />
@@ -127,9 +123,9 @@ const ContactForm = () => {
               </div>
             ) : (
               <Skeleton
-                height={"1.9rem"}
-                style={{ minWidth: "25rem" }}
-                width={"100%"}
+                height={'1.9rem'}
+                style={{ minWidth: '25rem' }}
+                width={'100%'}
                 highlightColor={highlightColor}
               />
             )}
@@ -138,8 +134,7 @@ const ContactForm = () => {
                 className="contact-form-link-left"
                 target="_blank"
                 rel="noreferrer"
-                href={"tel:" + contact[0].phone}
-              >
+                href={'tel:' + contact[0].phone}>
                 <div className="link-icon">
                   <img src={phone} alt="" />
                 </div>
@@ -148,9 +143,9 @@ const ContactForm = () => {
             ) : (
               <Skeleton
                 highlightColor={highlightColor}
-                height={"1.9rem"}
-                style={{ minWidth: "12rem" }}
-                width={"100%"}
+                height={'1.9rem'}
+                style={{ minWidth: '12rem' }}
+                width={'100%'}
               />
             )}
           </div>
@@ -161,66 +156,38 @@ const ContactForm = () => {
                   href={contact[0].instagram}
                   target="_blank"
                   rel="noreferrer"
-                  className="link-icon"
-                >
+                  className="link-icon">
                   <img src={ig} alt="" />
                 </a>
               ) : (
-                <Skeleton
-                  height={"3rem"}
-                  width={"3rem"}
-                  highlightColor={highlightColor}
-                />
+                <Skeleton height={'3rem'} width={'3rem'} highlightColor={highlightColor} />
               )}
               {contact[0].id !== -1 ? (
-                <a
-                  href={contact[0].twitter}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="link-icon"
-                >
+                <a href={contact[0].twitter} target="_blank" rel="noreferrer" className="link-icon">
                   <img src={twitter} alt="" />
                 </a>
               ) : (
-                <Skeleton
-                  height={"3rem"}
-                  width={"3rem"}
-                  highlightColor={highlightColor}
-                />
+                <Skeleton height={'3rem'} width={'3rem'} highlightColor={highlightColor} />
               )}
             </div>
             <div className="contact-form-link-right">
               {contact[0].id !== -1 ? (
-                <a
-                  href={contact[0].youtube}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="link-icon"
-                >
+                <a href={contact[0].youtube} target="_blank" rel="noreferrer" className="link-icon">
                   <img src={yt} alt="" />
                 </a>
               ) : (
-                <Skeleton
-                  height={"3rem"}
-                  width={"3rem"}
-                  highlightColor={highlightColor}
-                />
+                <Skeleton height={'3rem'} width={'3rem'} highlightColor={highlightColor} />
               )}
               {contact[0].id !== -1 ? (
                 <a
                   href={contact[0].facebook}
                   target="_blank"
                   rel="noreferrer"
-                  className="link-icon"
-                >
+                  className="link-icon">
                   <img src={fb} alt="" />
                 </a>
               ) : (
-                <Skeleton
-                  height={"3rem"}
-                  width={"3rem"}
-                  highlightColor={highlightColor}
-                />
+                <Skeleton height={'3rem'} width={'3rem'} highlightColor={highlightColor} />
               )}
             </div>
           </div>
@@ -250,11 +217,8 @@ const ContactForm = () => {
               />
               <span
                 className={
-                  alert.name && data.name.length === 0
-                    ? "form-warning active"
-                    : "form-warning"
-                }
-              >
+                  alert.name && data.name.length === 0 ? 'form-warning active' : 'form-warning'
+                }>
                 Это поле обязательно для заполнения
               </span>
             </div>
@@ -281,11 +245,8 @@ const ContactForm = () => {
               />
               <span
                 className={
-                  alert.email && !validateEmail(data.email)
-                    ? "form-warning active"
-                    : "form-warning"
-                }
-              >
+                  alert.email && !validateEmail(data.email) ? 'form-warning active' : 'form-warning'
+                }>
                 Введен некорректный email
               </span>
             </div>
@@ -305,15 +266,13 @@ const ContactForm = () => {
                 onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
                   setAlert({ ...alert, message: true });
                   setData({ ...data, message: e.target.value });
-                }}
-              ></textarea>
+                }}></textarea>
               <span
                 className={
                   alert.message && data.message.length === 0
-                    ? "form-warning active"
-                    : "form-warning"
-                }
-              >
+                    ? 'form-warning active'
+                    : 'form-warning'
+                }>
                 Это поле обязательно для заполнения
               </span>
             </div>
@@ -325,13 +284,11 @@ const ContactForm = () => {
               setLoad(true);
               e.preventDefault();
               sendData(setLoad, setSent, setTriggered, data);
-            }}
-          >
+            }}>
             Отправить сообщение
           </button>
           <span className="form-note">
-            Поля отмеченные <span className="red">*</span> обязательны для
-            заполнения
+            Поля отмеченные <span className="red">*</span> обязательны для заполнения
           </span>
           <Status message={status} success={sent} active={triggered} />
         </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
 // vite.config.ts
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'; // Import the React plugin
 
 export default defineConfig({
-  // Base URL for your app (adjust as needed)
-  base: '/subscriber/',
-
   // Plugins
   plugins: [react()], // Use the Vite React plugin
 
