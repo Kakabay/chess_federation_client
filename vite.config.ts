68aa94de5d147c76334184c5cb2db3409d697f65
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-03-06 15:39:27 +0500

    vite config chaged

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.ts
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'; // Import the React plugin
+
 export default defineConfig({
-  plugins: [react()],
+  // Base URL for your app (adjust as needed)
+  base: '/subscriber/',
+
+  // Plugins
+  plugins: [react()], // Use the Vite React plugin
+
+  // Other configuration options
+  build: {
+    // Output directory (adjust as needed)
+    outDir: 'dist',
+  },
 });
