8793147b9e6032dee4738a6924a7984a3e557bb7
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-03-16 16:08:05 +0500

    section title refactored

--- src/components/global/SectionTitle.tsx
+++ src/components/global/SectionTitle.tsx
@@ -1,23 +1,48 @@
 // Modules
 
 // Icons
-import bishop from "../../icons/bishop.svg";
+import bishop from '../../icons/bishop.svg';
+import king from '../../icons/king.svg';
+import rook from '../../icons/rook.svg';
+import horse from '../../icons/horse.svg';
+import arrRight from '../../icons/arr-right.svg';
+import { Link } from 'react-router-dom';
 
 interface Props {
   title: string;
+  icon: 'rook' | 'horse' | 'king' | 'bishop';
+  link?: string;
+  linkName?: string;
 }
 
-const SectionTitle = ({ title }: Props) => {
+const SectionTitle = ({ title, icon, link, linkName }: Props) => {
   return (
     <div className="section-title">
-      <div className="section-title-img">
-        <img src={bishop} alt="" />
+      <div className="section-title-inner">
+        <div className="section-title-img">
+          <img
+            src={
+              icon === 'bishop'
+                ? bishop
+                : icon === 'horse'
+                ? horse
+                : icon === 'king'
+                ? king
+                : icon === 'rook'
+                ? rook
+                : bishop
+            }
+            alt=""
+          />
+        </div>
+        <h2 className="section-title-title">{title}</h2>
       </div>
-      <h2 className="section-title-title">{title}</h2>
-      <div className="section-title-deco">
-        <div className="deco-rotated"></div>
-        <div className="deco"></div>
-      </div>
+      {link && linkName ? (
+        <Link to={link} className="section-title-link">
+          {linkName}
+          <img src={arrRight} alt="chevron" />
+        </Link>
+      ) : null}
     </div>
   );
 };
