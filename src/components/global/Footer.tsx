daaf968f15ec1acabcbb769df139e194c7ace764
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-03-18 23:24:37 +0500

    footer finished

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -6,87 +6,46 @@ import vk from "../../icons/vk.svg";
 import instagram from "../../icons/instagram.svg";
 import facebook from "../../icons/facebook.svg";
 
-// Types
-import { footerProps } from "../../types/burger";
+const footerLinks = [
+  {
+    title: "О нас",
+    path: "/about-us",
+  },
+  {
+    title: "Новости",
+    path: "/about-us",
+  },
+  {
+    title: "Прошедшие события",
+    path: "/about-us",
+  },
+  {
+    title: "Контакты",
+    path: "/about-us",
+  },
+];
 
-// Hooks
-import useMediaQuery from "../../hooks/useMediaQuery";
-
-const Footer = ({
-  setDropdown,
-  setDropdownBurger,
-  setBurgerOpen,
-  refTrig_1,
-}: footerProps) => {
-  const date = new Date();
-  const breakpoint: Record<number, boolean> = {
-    1300: useMediaQuery("(max-width: 1300px)"),
-  };
-
+const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-top">
-        <div className="container">
-          <div className="footer-top-inner">
-            <ul>
-              <li
-                ref={refTrig_1}
-                onClick={() => {
-                  if (breakpoint["1300"]) {
-                    window.scrollTo(0, 0);
-                    setBurgerOpen(true);
-                    setDropdownBurger(true);
-                  } else {
-                    window.scrollTo(0, 0);
-                    setDropdown(true);
-                  }
-                }}
-              >
-                <span>О нас</span>
-              </li>
-              <li>
-                <Link to={"/news"}>Новости и cобытия</Link>
-              </li>
-              <li>
-                <Link to={"/tournaments"}>Турниры</Link>
-              </li>
-              <li>
-                <Link to={"/rating"}>Рейтинг</Link>
-              </li>
-              <li>
-                <Link to={"/contact"}>Контакты</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div className="footer-bottom">
-        <div className="container">
-          <div className="footer-bottom-inner">
-            <div className="footer-bottom-left">
-              <p>
-                Copyright 2012-{date.getFullYear()} Шахматная федерация
-                Туркменистана | <a href="/">Пользовательское соглашение</a>
-              </p>
+      <div className="container">
+        <div className="footer-inner">
+          <nav className="footer-nav">
+            {footerLinks.map((link) => (
+              <Link to={link.path} className="footer-nav-link">
+                {link.title}
+              </Link>
+            ))}
+          </nav>
+          <div className="footer-info">
+            <div className="footer-info-top">
+              <h4>Пользовательское соглашение</h4>
+              <h4>Copyright 2012-2024. Шахматная федерация Туркменистана</h4>
             </div>
-            <div className="footer-bottom-right">
-              <ul>
-                <li>
-                  <a href="/">
-                    <img src={vk} alt="" />
-                  </a>
-                </li>
-                <li>
-                  <a href="/">
-                    <img src={instagram} alt="" />
-                  </a>
-                </li>
-                <li>
-                  <a href="/">
-                    <img src={facebook} alt="" />
-                  </a>
-                </li>
-              </ul>
+            <div className="footer-info-bottom">
+              <img src={vk} alt="vk" />
+              <img src={instagram} alt="inst" />
+              <img src={facebook} alt="facebook" />
             </div>
           </div>
         </div>
