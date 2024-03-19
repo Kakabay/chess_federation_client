9d957f8c849145e6fe04c60602cb9affdebca046
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-03-19 16:09:52 +0500

    tournaments data fetched

--- src/components/global/TournamentItem.tsx
+++ src/components/global/TournamentItem.tsx
@@ -1,22 +1,34 @@
-const TournamentItem = () => {
+import { dateParser } from "../../helpers/dateParser";
+
+interface TournamentItemProps {
+  startTime: string;
+  endTime: string;
+  place: string;
+  tourName: string;
+}
+
+const TournamentItem = ({
+  startTime,
+  endTime,
+  place,
+  tourName,
+}: TournamentItemProps) => {
   return (
     <div className="tournament-item">
       <div className="tournament-item-date">
         <h4 className="tournament-item-date-item">
-          <span>4</span>
-          марта
+          <span>{dateParser(startTime, false)[0]}</span>
+          {dateParser(startTime, false)[1]}
         </h4>
         <div className="tournament-item-date-divider"></div>
         <h4 className="tournament-item-date-item">
-          <span>4</span>
-          марта
+          <span>{dateParser(startTime, false)[0]}</span>
+          {dateParser(startTime, false)[1]}
         </h4>
       </div>
       <div className="tournament-item-content">
-        <h3 className="tournament-item-title">
-          Высшая лига Чемпионата Туркменистана среди мужчин и женщин.
-        </h3>
-        <h4 className="tournament-item-subtitle">Шахматно-шашечная школа, г. Ашхабад</h4>
+        <h3 className="tournament-item-title">{tourName}</h3>
+        <h4 className="tournament-item-subtitle">{place}</h4>
       </div>
     </div>
   );
--- src/components/main/TournamentsSection.tsx
+++ src/components/main/TournamentsSection.tsx
@@ -1,15 +1,87 @@
-import SectionTitle from '../global/SectionTitle';
-import TournamentItem from '../global/TournamentItem';
+import { useEffect, useState } from "react";
+import { getEvents } from "../../helpers/apiRequests";
+import SectionTitle from "../global/SectionTitle";
+import TournamentItem from "../global/TournamentItem";
+import { Tournaments } from "../../types/tournaments";
+import { v4 } from "uuid";
 
 const TournamentsSection = () => {
+  const months = [
+    {
+      number: 1,
+      name: "Январь",
+    },
+    {
+      number: 2,
+      name: "Февраль",
+    },
+    {
+      number: 3,
+      name: "Март",
+    },
+    {
+      number: 4,
+      name: "Апрель",
+    },
+    {
+      number: 5,
+      name: "Май",
+    },
+    {
+      number: 6,
+      name: "Июнь",
+    },
+    {
+      number: 7,
+      name: "Июль",
+    },
+    {
+      number: 8,
+      name: "Август",
+    },
+    {
+      number: 9,
+      name: "Сентябрь",
+    },
+    {
+      number: 10,
+      name: "Октябрь",
+    },
+    {
+      number: 11,
+      name: "Ноябрь",
+    },
+    {
+      number: 12,
+      name: "Декабрь",
+    },
+  ];
+
+  const [tourData, setTourData] = useState<Tournaments>();
+
+  useEffect(() => {
+    getEvents(setTourData);
+  }, []);
+
+  console.log(tourData?.data);
+
   return (
     <section className="tournaments">
       <div className="container">
         <div className="tournaments-inner">
           <SectionTitle icon="horse" title="Предстоящие события" />
           <div className="tournaments-content">
-            <TournamentItem />
-            <TournamentItem />
+            {tourData?.data
+              ? tourData.data[0].events.map((tour) => (
+                  <TournamentItem
+                    endTime={tour.end}
+                    place={tour.place}
+                    tourName={tour.name}
+                    startTime={tour.start}
+                    key={v4()}
+                  />
+                ))
+              : null}
           </div>
         </div>
       </div>
